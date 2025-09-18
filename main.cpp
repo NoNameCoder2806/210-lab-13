a708c947e51ddc7612e3a10dd3100b0c86edb541
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-18 16:33:32 -0700

    Changing the arrays to vectors...

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// COMSC-210 | Lab 12 | Dat Hoang Vien
+// COMSC-210 | Lab 13 | Dat Hoang Vien
 // IDE used: Visual Code Studio
 
 // Libraries
@@ -8,33 +8,28 @@
 #include <algorithm>  // for sort(), find()
 #include <numeric>    // for accumulate()
 #include <string>
-#include <array>
+#include <vector>
 using namespace std;
 
-// Constants
-const int ARRAY_SIZE = 50;
-const int SMALL_ARRAY_SIZE = 5;
-
 // Function prototypes
-void fileInput(array<double, ARRAY_SIZE>& gpaArray, string path);
+void fileInput(vector<double>& gpaVector, string path);
 
-template <size_t N>
-void displayArray(const array<double, N>& gpaArray);
+void displayArray(const vector<double>& gpaVector);
 
-void findElement(array<double, ARRAY_SIZE>& gpaArray, double targetGPA);
+//void findElement(array<double, ARRAY_SIZE>& gpaArray, double targetGPA);
 
 // Main
 int main()
 {
-    // Declare an array to store double gpa data
-    array<double, ARRAY_SIZE> gpaArray;
+    // Declare a vector to store double gpa data
+    vector<double> gpaVector;
 
     // Output the address to the console
-    cout << " - Address of the array: " << gpaArray.data() << endl;
+    cout << " - Address of the vector: " << gpaVector.data() << endl;
 
     // Check whether the current array is empty
     cout << " - Empty?: ";
-    if (gpaArray.empty())
+    if (gpaVector.empty())
     {
         cout << "True!" << endl;
     }
@@ -44,75 +39,75 @@ int main()
     }
 
     // Read the data from the file
-    fileInput(gpaArray, "data.txt");
+    fileInput(gpaVector, "data.txt");
 
     // Print the current size
-    cout << " - Array size: " << gpaArray.size() << endl;
+    cout << " - Array size: " << gpaVector.size() << endl;
     cout << endl;
     
-    // Display all the gpa in the array
+    // Display all the gpa in the vector
     cout << "Original array: ";
-    displayArray(gpaArray);
+    displayArray(gpaVector);
     cout << endl;
 
-    // Display the first and last element of the array
-    cout << " - First element: " << gpaArray.front() << endl;
-    cout << " - Last element: " << gpaArray.back() << endl;
+    // Display the first and last element of the vector
+    cout << " - First element: " << gpaVector.front() << endl;
+    cout << " - Last element: " << gpaVector.back() << endl;
     cout << endl;
 
-    // Sort the array
-    sort(gpaArray.begin(), gpaArray.end());
+    // Sort the vector
+    sort(gpaVector.begin(), gpaVector.end());
 
-    // Display the sorted array
-    cout << "Sorted array: ";
-    displayArray(gpaArray);
+    // Display the sorted vector
+    cout << "Sorted vector: ";
+    displayArray(gpaVector);
     cout << endl;
 
-    // Declare a few GPA scores to search for
-    double target1 = 2.25;
-    double target2 = 2.5;
-    double target3 = 2.75;
-    double target4 = 3.0;
+    // // Declare a few GPA scores to search for
+    // double target1 = 2.25;
+    // double target2 = 2.5;
+    // double target3 = 2.75;
+    // double target4 = 3.0;
 
-    array<double, ARRAY_SIZE>::iterator it;
+    // array<double, ARRAY_SIZE>::iterator it;
 
-    // Check whether they are in the array or not
-    findElement(gpaArray, target1);
-    findElement(gpaArray, target2);
-    findElement(gpaArray, target3);
-    findElement(gpaArray, target4);
+    // // Check whether they are in the array or not
+    // findElement(gpaArray, target1);
+    // findElement(gpaArray, target2);
+    // findElement(gpaArray, target3);
+    // findElement(gpaArray, target4);
 
-    // Find the min, max, and average GPA score of the Student objects
-    cout << " - Highest GPA: " << *max_element(gpaArray.begin(), gpaArray.end()) << endl;
-    cout << " - Lowest GPA: " << *min_element(gpaArray.begin(), gpaArray.end()) << endl;
-    cout << " - Average GPA: " << accumulate(gpaArray.rbegin(), gpaArray.rend(), 0.0) / gpaArray.size() << endl;
-    cout << endl;
+    // // Find the min, max, and average GPA score of the Student objects
+    // cout << " - Highest GPA: " << *max_element(gpaArray.begin(), gpaArray.end()) << endl;
+    // cout << " - Lowest GPA: " << *min_element(gpaArray.begin(), gpaArray.end()) << endl;
+    // cout << " - Average GPA: " << accumulate(gpaArray.rbegin(), gpaArray.rend(), 0.0) / gpaArray.size() << endl;
+    // cout << endl;
 
-    // Create 2 empty arrays
-    array<double, SMALL_ARRAY_SIZE> fours;
-    array<double, SMALL_ARRAY_SIZE> twos;
+    // // Create 2 empty arrays
+    // array<double, SMALL_ARRAY_SIZE> fours;
+    // array<double, SMALL_ARRAY_SIZE> twos;
 
-    // Fill the Student objects into the array
-    fours.fill(4.0);
-    twos.fill(2.0);
+    // // Fill the Student objects into the array
+    // fours.fill(4.0);
+    // twos.fill(2.0);
 
-    // Display the 2 arrays
-    cout << "Array of Fours: ";
-    displayArray(fours);
-    cout << "Array of Twos: ";
-    displayArray(twos);
-    cout << endl;
+    // // Display the 2 arrays
+    // cout << "Array of Fours: ";
+    // displayArray(fours);
+    // cout << "Array of Twos: ";
+    // displayArray(twos);
+    // cout << endl;
 
-    // Swap the 2 arrays
-    fours.swap(twos);
-    cout << " --- The 2 arrays are swapped! --- " << endl;
+    // // Swap the 2 arrays
+    // fours.swap(twos);
+    // cout << " --- The 2 arrays are swapped! --- " << endl;
 
-    // Display the 2 arrays again
-    cout << "Array of Fours: ";
-    displayArray(fours);
-    cout << "Array of Twos: ";
-    displayArray(twos);
-    cout << endl;
+    // // Display the 2 arrays again
+    // cout << "Array of Fours: ";
+    // displayArray(fours);
+    // cout << "Array of Twos: ";
+    // displayArray(twos);
+    // cout << endl;
 
     return 0;
 }
@@ -122,11 +117,11 @@ int main()
     fileInput()
     Add new gpa from a file
     Arguments: 
-        - gpaArray: reference to the std::array<double, ARRAY_SIZE> that will store the data
+        - gpaVector: reference to the std::array<double> that will store the data
         - path: path to the data file
     Returns: none
 */
-void fileInput(array<double, ARRAY_SIZE>& gpaArray, string path)
+void fileInput(vector<double>& gpaVector, string path)
 {
     // Create a file stream and read the file
     ifstream fin;
@@ -141,38 +136,31 @@ void fileInput(array<double, ARRAY_SIZE>& gpaArray, string path)
     // Declare a string variable to store the line
     double value = 0.0;
 
-    // Fill the array with 0.0 before reading
-    gpaArray.fill(value);
-
     // Iterate through the file and read the data line by line
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    while (fin >> value)
     {
-        // Read the value
-        fin >> value;
-
         // Add the value into the array
-        gpaArray.at(i) = value;
+        gpaVector.push_back(value);
     }
 }
 
 /*
-    displayArray()
-    Display the data of the gpa (double type) in the array
+    displayVector()
+    Display the data of the gpa (double type) in the vector
     Arguments:
-        - gpaArray: const reference to the std::array<double, ARRAY_SIZE> containing gpa values
+        - gpaVector: const reference to the std::vector<double> containing gpa values
     Returns: none
 */
-template <size_t N>
-void displayArray(const array<double, N>& gpaArray)
+void displayArray(const vector<double>& gpaVector)
 {
-    // Iterate through the array
-    for (int i = 0; i < N; i++)
+    // Iterate through the vector
+    for (int i = 0; i < gpaVector.size(); i++)
     {
         // Display the fields
-        cout << gpaArray[i];
+        cout << gpaVector[i];
 
         // Add a comma and a space if this is not the last element
-        if (i != N - 1)
+        if (i != gpaVector.size() - 1)
         {
             cout << ", ";
         }
@@ -191,27 +179,27 @@ void displayArray(const array<double, N>& gpaArray)
     Returns:
         - none (prints search results to the console)
 */
-void findElement(array<double, ARRAY_SIZE>& gpaArray, double targetGPA)
-{
-    // Display a message to notify the user of the element being searched
-    cout << " --- Searching for: " << targetGPA << " --- " << endl;
+// void findElement(array<double, ARRAY_SIZE>& gpaArray, double targetGPA)
+// {
+//     // Display a message to notify the user of the element being searched
+//     cout << " --- Searching for: " << targetGPA << " --- " << endl;
 
-    // Declare an iterator to point to the found element
-    array<double, ARRAY_SIZE>::iterator it;
-    it = find(gpaArray.begin(), gpaArray.end(), targetGPA);
+//     // Declare an iterator to point to the found element
+//     array<double, ARRAY_SIZE>::iterator it;
+//     it = find(gpaArray.begin(), gpaArray.end(), targetGPA);
     
-    // Check whether the iterator points to an element or not
-    if (it != gpaArray.end())
-    {
-        // Displaying a message
-        cout << "Element found! Position: " << it - gpaArray.begin() << endl;
-    }
-    else
-    {
-        // Displaying a message
-        cout << "Element not found!" << endl;
-    }
+//     // Check whether the iterator points to an element or not
+//     if (it != gpaArray.end())
+//     {
+//         // Displaying a message
+//         cout << "Element found! Position: " << it - gpaArray.begin() << endl;
+//     }
+//     else
+//     {
+//         // Displaying a message
+//         cout << "Element not found!" << endl;
+//     }
 
-    // Enter a new line
-    cout << endl;
-}
+//     // Enter a new line
+//     cout << endl;
+// }
